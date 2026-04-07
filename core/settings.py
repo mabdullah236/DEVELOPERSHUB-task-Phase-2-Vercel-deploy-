d5cde4a42a79c6bf1352c8d3d59c2a14053b4517
muhammad abdullah <mabdullah236>
2026-04-07 15:47:01 +0500

    Cleaned settings and ready for Vercel

--- core/settings.py
+++ core/settings.py
@@ -24,9 +24,6 @@ env_path = BASE_DIR / '.env'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r4-pse6x2(ag3k(o6@nufp+zbr^a&=)_i8z*8mlzt_uy=r=i0_'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
